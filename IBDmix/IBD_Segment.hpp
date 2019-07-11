d2332c193d00d0dee1c54c2b54e2678524af6076
Troy Comi <troycomi@gmail.com>
2019-07-11 09:22:38 -0400

    Update IBD Segment to match legacy
    Changed IBD Segment output to match the legacy version.  Also added a
    purge function to

--- IBDmix/IBD_Segment.hpp
+++ IBDmix/IBD_Segment.hpp
@@ -5,16 +5,17 @@
 class IBD_Segment{
     private:
         struct IBD_Node *start, *end, *top;
+        char *name;
         int chrom;
         double thresh;
-        void add_node(struct IBD_Node *new_node, FILE * output);
+        void add_node(struct IBD_Node *new_node, FILE *output);
 
     public:
         IBD_Segment(const char *name, double threshold);
         ~IBD_Segment();
         void add_lod(int chromosome, unsigned long int position,
                 double lod, FILE * output);
+        void purge(FILE * output);
         void display(void);
-        char *name;
         int length(void);
 };
--- tests/test_IBD_segment.cpp
+++ tests/test_IBD_segment.cpp
@@ -59,18 +59,20 @@ BOOST_AUTO_TEST_CASE(test_add_lod_output){
     BOOST_REQUIRE_EQUAL(seg.length(), 2);
 
     // make cumsum 0
-    seg.add_lod(2, 5, -0.4, output);
+    seg.add_lod(2, 50, -0.4, output);
     BOOST_REQUIRE_EQUAL(seg.length(), 3);
     // and less than 0
-    seg.add_lod(2, 6, -0.1, output);
-    BOOST_REQUIRE_EQUAL(output, "test\t2\t1\t4\t0.4\n");
+    seg.add_lod(2, 60, -0.1, output);
+    // TODO expect output to be the next node's position to match legacy
+    // will probably want to change!
+    BOOST_REQUIRE_EQUAL(output, "test\t2\t1\t50\t0.4\n");
     BOOST_REQUIRE_EQUAL(seg.length(), 0);
     BOOST_REQUIRE_EQUAL(pool_length(), pool);
 
     //have start = end
     seg.add_lod(2, 1, 2, output);
     seg.add_lod(2, 2, -3, output);
-    BOOST_REQUIRE_EQUAL(output, "test\t2\t1\t1\t2\n");
+    BOOST_REQUIRE_EQUAL(output, "test\t2\t1\t2\t2\n");
     BOOST_REQUIRE_EQUAL(seg.length(), 0);
     BOOST_REQUIRE_EQUAL(pool_length(), pool);
 
@@ -82,7 +84,7 @@ BOOST_AUTO_TEST_CASE(test_add_lod_output){
     seg.add_lod(2, 5, 0.7, output);
     seg.add_lod(2, 6, -2, output);
     BOOST_REQUIRE_EQUAL(output,
-            "test\t2\t1\t1\t2\ntest\t2\t3\t3\t0.5\ntest\t2\t5\t5\t0.7\n");
+            "test\t2\t1\t2\t2\ntest\t2\t3\t4\t0.5\ntest\t2\t5\t6\t0.7\n");
     BOOST_REQUIRE_EQUAL(seg.length(), 0);
     BOOST_REQUIRE_EQUAL(pool_length(), pool);
 
@@ -95,7 +97,7 @@ BOOST_AUTO_TEST_CASE(test_add_lod_output){
     seg.add_lod(2, 6, 0.3, output);
     seg.add_lod(2, 7, -2, output);
     BOOST_REQUIRE_EQUAL(output,
-            "test\t2\t1\t1\t2\ntest\t2\t3\t3\t0.5\ntest\t2\t5\t6\t0.6\n");
+            "test\t2\t1\t2\t2\ntest\t2\t3\t4\t0.5\ntest\t2\t5\t7\t0.6\n");
     BOOST_REQUIRE_EQUAL(seg.length(), 0);
     BOOST_REQUIRE_EQUAL(pool_length(), pool);
 
@@ -111,9 +113,30 @@ BOOST_AUTO_TEST_CASE(test_add_lod_output){
     seg.add_lod(2, 9, 0.5, output);
     seg.add_lod(2, 10, -3, output);
     BOOST_REQUIRE_EQUAL(output,
-            "test\t2\t1\t1\t2\ntest\t2\t3\t3\t0.5\n"
-            "test\t2\t5\t5\t0.5\ntest\t2\t7\t7\t1.9\n"
-            "test\t2\t9\t9\t0.5\n");
+            "test\t2\t1\t2\t2\ntest\t2\t3\t4\t0.5\n"
+            "test\t2\t5\t6\t0.5\ntest\t2\t7\t8\t1.9\n"
+            "test\t2\t9\t10\t0.5\n");
     BOOST_REQUIRE_EQUAL(seg.length(), 0);
     BOOST_REQUIRE_EQUAL(pool_length(), pool);
+}
+
+BOOST_AUTO_TEST_CASE(test_purge){
+    char output[100];
+    output[0] = '\0';
+    IBD_Segment seg = IBD_Segment("test", 0);
+    seg.add_lod(2, 1, 2, output);
+    seg.add_lod(2, 2, -1, output);
+    seg.add_lod(2, 3, 0.5, output);
+    seg.add_lod(2, 4, -1, output);
+    seg.add_lod(2, 5, 0.5, output);
+    seg.add_lod(2, 6, -1, output);
+    seg.add_lod(2, 7, 1.9, output);
+    seg.add_lod(2, 8, -1, output);
+    seg.add_lod(2, 9, 0.5, output);
+    BOOST_REQUIRE_EQUAL('\0', output[0]);
+    seg.purge(output);
+    BOOST_REQUIRE_EQUAL(output,
+            "test\t2\t1\t2\t2\ntest\t2\t3\t4\t0.5\n"
+            "test\t2\t5\t6\t0.5\ntest\t2\t7\t8\t1.9\n"
+            "test\t2\t9\t9\t0.5\n");
 }
