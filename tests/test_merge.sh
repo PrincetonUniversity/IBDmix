4873f7f1fb61a5c7a0e8bc817855b263fbd6af87
Troy Comi <troycomi@gmail.com>
2019-07-12 14:02:46 -0400

    Fully match legacy generate gt
    Tested generate gt against entire altai_1kgp merge set (chromosome 1-22)
    and now matching.
    Changes included changing when lines were excluded from modern samples.

--- tests/test_merge.sh
+++ tests/test_merge.sh
@@ -1,35 +1,32 @@
 #!/usr/bin/bash
 
-MOD_INPUT_FILE=/tigress/AKEY/akey_vol2/wqfu/nobackup/1KGP/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz
-original_out=/tigress/AKEY/akey_vol1/home/luchenuw/data/VCFs/Merged_Archaic_1KGP3/2013pubAltai/Altai_1KGP3_chr1.txt
-ARCH_INPUT_FILE=/tigress/limingli/data/old-Altai/gz/chr1.vcf.gz
+MOD_INPUT_FILE=/tigress/AKEY/akey_vol2/wqfu/nobackup/1KGP/ALL.chr
+MOD_SUFF=.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz
+original_out=/tigress/AKEY/akey_vol1/home/luchenuw/data/VCFs/Merged_Archaic_1KGP3/2013pubAltai/Altai_1KGP3_chr
+ARCH_INPUT_FILE=/tigress/limingli/data/old-Altai/gz/chr
 CPP_OUT2=/tigress/tcomi/ibdmix_temp/alti_1kg.gz
 
 
 #/usr/bin/time -v bash -c "cat $MOD_INPUT_FILE | ./mergeVCF -a $ARCH_INPUT_FILE -l $(wc -l <$ARCH_INPUT_FILE) -d 1 -n 279 -o $CPP_OUT"
 #mv gmon.out gmon_old.out
 g++ -std=c++11 IBDmix/generate_gt.cpp -o tests/generate_gt
-tests/generate_gt \
-    -a <(zcat $ARCH_INPUT_FILE | head -1000) \
-    -m <(zcat $MOD_INPUT_FILE | head -1000) \
-    -o - | gzip > $CPP_OUT2
-line=$(cmp <(zcat $CPP_OUT2 | tail -n +2) $original_out | awk '{print $NF}')
-if [ ! -z $line ]; then
-    awk -v line=$line 'NR==line{print "Expected: "$1, $2, $3, $4, $5; exit}' "$original_out"
-    zcat $CPP_OUT2 | awk -v line=$line 'NR==line+1{print "Actual: "$1, $2, $3, $4, $5; exit}'
-else
-    echo "Matching!"
-fi
-exit
 
-EX=$CPP_OUT
-ACT=${CPP_OUT2}.tail
-tail -n +2 $CPP_OUT2 > $ACT
+    #-a <(zcat $ARCH_INPUT_FILE | head -100000) \
+for chr in {1..22}; do
+    echo "starting on $chr"
+    line=$(cmp <(tests/generate_gt \
+        -a <(zcat ${ARCH_INPUT_FILE}${chr}.vcf.gz) \
+        -m <(zcat $MOD_INPUT_FILE$chr$MOD_SUFF) \
+        -o - | tee >(gzip > $CPP_OUT2) \
+            | tail -n +2) \
+        ${original_out}${chr}.txt | awk '{print $NF}')
 
-line=$(cmp "$ACT" "$EX" | awk '{print $NF}')
-if [ ! -z $line ]; then
-    awk -v file="$EX" -v line=$line 'NR==line{print "In file "file": "$1, $2, $3, $4, $5; exit}' "$EX"
-    awk -v file="$ACT" -v line=$line 'NR==line{print "In file "file": "$1, $2, $3, $4, $5; exit}' "$ACT"
-else
-    echo "Matching!"
-fi
+    if [ ! -z $line ]; then
+        echo "MISMATCH! Line: $line"
+        awk -v line=$line 'NR==line{print "Expected: "$1, $2, $3, $4, $5; exit}' "${original_out}${chr}.txt"
+        zcat $CPP_OUT2 | awk -v line=$line 'NR==line+1{print "Actual: "$1, $2, $3, $4, $5; exit}'
+        exit
+    else
+        echo "Matching with chromosome ${chr}!"
+    fi
+done
