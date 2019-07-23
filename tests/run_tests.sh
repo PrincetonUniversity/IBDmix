ade79240dee9b0b5b632cab4f64c216e2bf63377
Troy Comi <troycomi@gmail.com>
2019-07-23 11:36:36 -0400

    Clean up directory
    Removed old files and consolidated test scripts

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,24 @@
 module load boost
-g++ test_mergeVCF.cpp -std=c++11 -lboost_system -lboost_thread -lboost_unit_test_framework -o utest
-./utest
-exit
-g++ tests/test_FUIntroSeg.cpp -lboost_system -lboost_thread -lboost_unit_test_framework -o utest
-./utest
+set -euo pipefail
+g++ tests/test_mergeVCF.cpp -std=c++11 -lboost_system -lboost_thread -lboost_unit_test_framework -o tests/merge
+tests/merge
+
+g++ IBDmix/IBD_Stack.cpp tests/test_IBD_node.cpp \
+    -std=c++11 \
+    -o tests/node -lboost_system -lboost_thread -lboost_unit_test_framework
+tests/node
+
+g++ IBDmix/IBD_Stack.cpp IBDmix/IBD_Segment.cpp tests/test_IBD_segment.cpp \
+    -std=c++11 \
+    -o tests/segment -lboost_system -lboost_thread -lboost_unit_test_framework
+tests/segment
+
+g++ IBDmix/IBD_Stack.cpp IBDmix/IBD_Segment.cpp tests/test_IBD_segment_single.cpp \
+    -std=c++11 \
+    -o tests/single -lboost_system -lboost_thread -lboost_unit_test_framework
+tests/single
+
+g++ IBDmix/Genotype_Reader.cpp tests/test_Genotype_Reader.cpp \
+    -std=c++11 \
+    -o tests/reader -lboost_system -lboost_thread -lboost_unit_test_framework
+tests/reader
